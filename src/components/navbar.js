8b8bac856f48d2152a4b262e03c729acccfb7ea8
twisha-a <66455753+twisha-a@users.noreply.github.com>
2021-03-23 09:58:19 +0530

    navbar_responsive ready

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { FaBars, FaTimes, FaHandPeace } from "react-icons/fa";
 import { Button } from "./Button";
+import "./navbar.css";
+import { IconContext } from "react-icons/lib";
 
 function Navbar() {
   const [click, setClick] = useState(false);
@@ -22,62 +24,84 @@ function Navbar() {
 
   return (
     <>
-      <div className="navbar">
-        <div className="navbar-container container">
-          <Link to="/" className="navbar-logo">
-            <FaHandPeace className="navbar-icon" />
-            AT YOUR DOOR
-          </Link>
-          <div className="menu-icon" onClick={handleClick}>
-            {click ? <FaTimes /> : <FaBars />}
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <div className="navbar">
+          <div className="navbar-container container">
+            <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+              <FaHandPeace className="navbar-icon" />
+              AT YOUR DOOR
+            </Link>
+            <div className="menu-icon" onClick={handleClick}>
+              {click ? <FaTimes /> : <FaBars />}
+            </div>
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
+              <li className="nav-item">
+                <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/Market"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  Shop in our Market
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/Deliver"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  Want to Deliver Items
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/Sale"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  Sale and Offers
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/Sponsor"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  Sponsor
+                </Link>
+              </li>
+              <li className="nav-item" onClick={closeMobileMenu}>
+                <Link to="/Contact" className="nav-links">
+                  Contact Us
+                </Link>
+              </li>
+              <li className="nav-btn">
+                {button ? (
+                  <Link to="/sign-up" className="btn-link">
+                    <Button buttonStyle="btn--outline">Sign Up</Button>
+                  </Link>
+                ) : (
+                  <Link
+                    to="/sign-up"
+                    className="btn--link"
+                    onClick={closeMobileMenu}
+                  >
+                    <Button buttonStyle="btn--outline" buttonSize="btn--mobile">
+                      Sign up
+                    </Button>
+                  </Link>
+                )}
+              </li>
+            </ul>
           </div>
-          <ul className={click ? "nav-menu active" : "nav-menu"}>
-            <li className="nav-item">
-              <Link to="/" className="nav-links">
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/Market" className="nav-links">
-                Shop in our Market
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/Deliver" className="nav-links">
-                Want to Deliver Items
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/Sale" className="nav-links">
-                Sale and Offers
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/Sponsor" className="nav-links">
-                Sponsor
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/Contact" className="nav-links">
-                Contact Us
-              </Link>
-            </li>
-            <li className="nav-btn">
-              {button ? (
-                <Link to="/sign-up" className="btn-link">
-                  <Button buttonStyle="btn--outline">Sign Up</Button>
-                </Link>
-              ) : (
-                <Link to="/sign-up" className="btn--link">
-                  <Button buttonStyle="btn--outline" buttonSize="btn--mobile">
-                    Sign up
-                  </Button>
-                </Link>
-              )}
-            </li>
-          </ul>
         </div>
-      </div>
+      </IconContext.Provider>
     </>
   );
 }
